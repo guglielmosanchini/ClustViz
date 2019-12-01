deaf5099dff25def63f35fb9c3cc8da54cbfc2aa
guglielmosanchini <guglielmosanchini@gmail.com>
2019-12-01 19:44:26 +0100

    chameleon final

---
+++ algorithms/chameleon/visualization.py
@@ -0,0 +1,41 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from collections import Counter
+
+
+def plot2d_graph(graph, print_clust=True):
+    pos = nx.get_node_attributes(graph, 'pos')
+    colors = { 0:"seagreen", 1:'beige', 2:'yellow', 3:'grey',
+               4:'pink', 5:'turquoise', 6:'orange', 7:'purple', 8:'yellowgreen', 9:'olive', 10:'brown',
+               11:'tan', 12: 'plum', 13:'rosybrown', 14:'lightblue', 15:"khaki", 16:"gainsboro", 17:"peachpuff"}
+
+    el = nx.get_node_attributes(graph, 'cluster').values()
+    cmc = Counter(el).most_common()
+    c = [colors[i%17] for i in el]
+
+    if print_clust == True:
+        print("clusters: ", cmc)
+
+    if len(el) != 0:  # is set
+        nx.draw(graph, pos, node_color=c, node_size=60, edgecolors="black")
+    else:
+        nx.draw(graph, pos, node_size=60, edgecolors="black")
+    plt.show(block=False)
+
+
+def plot2d_data(df, col_i=None):
+
+    if (len(df.columns) > 3):
+        print("Plot Warning: more than 2 dimensions!")
+
+    df.plot(kind='scatter', c=df['cluster'], cmap='gist_rainbow', x=0, y=1)
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    if col_i is not None:
+        plt.scatter(df[df.cluster==col_i].iloc[:,0], df[df.cluster==col_i].iloc[:,1],
+                color="black", s=120, edgecolors="white", alpha=0.8)
+
+    plt.show(block=False)
