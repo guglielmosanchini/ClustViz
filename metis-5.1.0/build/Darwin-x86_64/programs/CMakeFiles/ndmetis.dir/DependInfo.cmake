b9aa5493b8d4b1fa702952e365ac432b1a82b246
guglielmosanchini <guglielmosanchini@gmail.com>
2020-06-09 16:29:05 +0200

    readded metis for Chameleon pytest

---
+++ metis-5.1.0/build/Darwin-x86_64/programs/CMakeFiles/ndmetis.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/ity9dw37/Desktop/metis-5.1.0/programs/cmdline_ndmetis.c" "/Users/ity9dw37/Desktop/metis-5.1.0/build/Darwin-x86_64/programs/CMakeFiles/ndmetis.dir/cmdline_ndmetis.c.o"
+  "/Users/ity9dw37/Desktop/metis-5.1.0/programs/io.c" "/Users/ity9dw37/Desktop/metis-5.1.0/build/Darwin-x86_64/programs/CMakeFiles/ndmetis.dir/io.c.o"
+  "/Users/ity9dw37/Desktop/metis-5.1.0/programs/ndmetis.c" "/Users/ity9dw37/Desktop/metis-5.1.0/build/Darwin-x86_64/programs/CMakeFiles/ndmetis.dir/ndmetis.c.o"
+  "/Users/ity9dw37/Desktop/metis-5.1.0/programs/smbfactor.c" "/Users/ity9dw37/Desktop/metis-5.1.0/build/Darwin-x86_64/programs/CMakeFiles/ndmetis.dir/smbfactor.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "SVNINFO=\"\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../GKlib"
+  "../../include"
+  "../../programs/../libmetis"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/ity9dw37/Desktop/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
