03a4e4244723b7feab722d18c9b5db42128d26b2
guglielmosanchini <guglielmosanchini@gmail.com>
2020-06-11 17:49:27 +0200

    deployment attempt nr 2b: correct url and version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="clustviz",
-    version="0.0.1",
+    version="0.0.2",
     author="Guglielmo Sanchini",
     author_email="guglielmosanchini@gmail.com",
     description="A 2D clustering algorithms visualization package",
