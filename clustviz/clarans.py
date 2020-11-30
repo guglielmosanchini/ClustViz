c3cccc171650f8183444c5f9c95f1e95a46fb53e
guglielmosanchini <guglielmosanchini@gmail.com>
2020-11-30 11:47:27 +0100

    added class inheritance for clarans of pyclustering

--- clustviz/clarans.py
+++ clustviz/clarans.py
@@ -1,29 +1,3 @@
-"""!
-
-@brief Cluster analysis algorithm: CLARANS.
-@details Implementation based on paper @cite article::clarans::1.
-
-@authors Andrei Novikov (pyclustering@yandex.ru)
-@date 2014-2019
-@copyright GNU Public License
-
-@cond GNU_PUBLIC_LICENSE
-    PyClustering is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    PyClustering is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-@endcond
-
-"""
-
 import random
 import scipy
 import itertools
@@ -31,71 +5,11 @@ import graphviz
 import numpy as np
 from clustviz.pam import plot_pam
 
-from pyclustering.cluster.encoder import type_encoding
-
 from pyclustering.utils import euclidean_distance_square
+from pyclustering.cluster.clarans import clarans as clarans_pyclustering
 
 
-class clarans:
-    """!
-    @brief Class represents clustering algorithm CLARANS (a method for clustering objects for spatial data mining).
-
-    """
-
-    def __init__(self, data, number_clusters, numlocal, maxneighbor):
-        """! @brief Constructor of clustering algorithm CLARANS. @details The higher the value of maxneighbor,
-        the closer is CLARANS to K-Medoids, and the longer is each search of a local minima.
-
-        @param[in] data (list): Input data that is presented as list of points (objects), each point should be
-        represented by list or tuple.
-        @param[in] number_clusters (uint): Amount of clusters that should be allocated.
-        @param[in] numlocal (uint): The number of local minima obtained (amount of iterations for solving the
-        problem).
-        @param[in] maxneighbor (uint): The maximum number of neighbors examined.
-
-        """
-
-        self.__pointer_data = data
-        self.__numlocal = numlocal
-        self.__maxneighbor = maxneighbor
-        self.__number_clusters = number_clusters
-
-        self.__clusters = []
-        self.__current = []
-        self.__belong = []
-
-        self.__optimal_medoids = []
-        self.__optimal_estimation = float("inf")
-
-        self.__verify_arguments()
-
-    def __verify_arguments(self):
-        """!
-        @brief Verify input parameters for the algorithm and throw exception in case of incorrectness.
-
-        """
-        if len(self.__pointer_data) == 0:
-            raise ValueError(
-                "Input data is empty (size: '%d')." % len(self.__pointer_data)
-            )
-
-        if self.__number_clusters <= 0:
-            raise ValueError(
-                "Amount of cluster (current value: '%d') for allocation should be greater than 0."
-                % self.__number_clusters
-            )
-
-        if self.__numlocal < 0:
-            raise ValueError(
-                "Local minima (current value: '%d') should be greater or equal to 0."
-                % self.__numlocal
-            )
-
-        if self.__maxneighbor < 0:
-            raise ValueError(
-                "Maximum number of neighbors (current value: '%d') should be greater or "
-                "equal to 0." % self.__maxneighbor
-            )
+class clarans(clarans_pyclustering):
 
     def process(self, plotting=False):
         """!
@@ -167,76 +81,6 @@ class clarans:
 
         return self
 
-    def get_clusters(self):
-        """!
-        @brief Returns allocated clusters by the algorithm.
-
-        @remark Allocated clusters can be returned only after data processing (use method process()), otherwise empty
-        list is returned.
-
-        @return (list) List of allocated clusters, each cluster contains indexes of objects in list of data.
-
-        @see process()
-        @see get_medoids()
-
-        """
-
-        return self.__clusters
-
-    def get_medoids(self):
-        """!
-        @brief Returns list of medoids of allocated clusters.
-
-        @see process()
-        @see get_clusters()
-
-        """
-
-        return self.__optimal_medoids
-
-    @staticmethod
-    def get_cluster_encoding():
-        """!
-        @brief Returns clustering result representation type that indicate how clusters are encoded.
-
-        @return (type_encoding) Clustering result representation.
-
-        @see get_clusters()
-
-        """
-
-        return type_encoding.CLUSTER_INDEX_LIST_SEPARATION
-
-    def __update_clusters(self, medoids):
-        """!
-        @brief Forms cluster in line with specified medoids by calculation distance from each point to medoids.
-
-        """
-
-        self.__belong = [0] * len(self.__pointer_data)
-        self.__clusters = [[] for _ in range(len(medoids))]
-        for index_point in range(len(self.__pointer_data)):
-            index_optim = -1
-            dist_optim = 0.0
-
-            for index in range(len(medoids)):
-                dist = euclidean_distance_square(
-                    self.__pointer_data[index_point],
-                    self.__pointer_data[medoids[index]],
-                )
-
-                if (dist < dist_optim) or (index == 0):
-                    index_optim = index
-                    dist_optim = dist
-
-            self.__clusters[index_optim].append(index_point)
-            self.__belong[index_point] = index_optim
-
-        # If cluster is not able to capture object it should be removed
-        self.__clusters = [
-            cluster for cluster in self.__clusters if len(cluster) > 0
-        ]
-
     def __optimize_configuration(self):
         """!
         @brief Finds quasi-optimal medoids and updates in line with them clusters in line with algorithm's rules.
@@ -292,7 +136,7 @@ class clarans:
                     # from the point to nearest (own) medoid
                     distance_nearest = float("inf")
                     if (point_medoid_index != candidate_medoid_index) and (
-                        point_medoid_index != current_medoid_cluster_index
+                            point_medoid_index != current_medoid_cluster_index
                     ):
                         distance_nearest = euclidean_distance_square(
                             self.__pointer_data[point_index],
@@ -304,13 +148,13 @@ class clarans:
                         # case 1:
                         if distance_candidate >= distance_nearest:
                             candidate_cost += (
-                                distance_nearest - distance_current
+                                    distance_nearest - distance_current
                             )
 
                         # case 2:
                         else:
                             candidate_cost += (
-                                distance_candidate - distance_current
+                                    distance_candidate - distance_current
                             )
 
                     elif point_cluster_index == other_medoid_cluster_index:
@@ -322,7 +166,7 @@ class clarans:
                         # case 4:
                         else:
                             candidate_cost += (
-                                distance_candidate - distance_nearest
+                                    distance_candidate - distance_nearest
                             )
 
             if candidate_cost < 0:
@@ -344,57 +188,10 @@ class clarans:
 
         print("Medoid set changed {0} times".format(counter))
 
-    def __find_another_nearest_medoid(self, point_index, current_medoid_index):
-        """!
-        @brief Finds the another nearest medoid for the specified point that is different from the specified medoid.
-
-        @param[in] point_index: index of point in dataspace for that searching of medoid in current list of medoids
-        is performed.
-        @param[in] current_medoid_index: index of medoid that shouldn't be considered as a nearest.
-
-        @return (uint) index of the another nearest medoid for the point.
-
-        """
-        other_medoid_index = -1
-        other_distance_nearest = float("inf")
-        for index_medoid in self.__current:
-            if index_medoid != current_medoid_index:
-                other_distance_candidate = euclidean_distance_square(
-                    self.__pointer_data[point_index],
-                    self.__pointer_data[current_medoid_index],
-                )
-
-                if other_distance_candidate < other_distance_nearest:
-                    other_distance_nearest = other_distance_candidate
-                    other_medoid_index = index_medoid
-
-        return other_medoid_index
-
-    def __calculate_estimation(self):
-        """!
-        @brief Calculates estimation (cost) of the current clusters. The lower the estimation,
-               the more optimally configuration of clusters.
-
-        @return (double) estimation of current clusters.
-
-        """
-        estimation = 0.0
-        for index_cluster in range(0, len(self.__clusters)):
-            cluster = self.__clusters[index_cluster]
-            index_medoid = self.__current[index_cluster]
-            for index_point in cluster:
-                estimation += euclidean_distance_square(
-                    self.__pointer_data[index_point],
-                    self.__pointer_data[index_medoid],
-                )
-
-        return estimation
-
 
 def compute_cost_clarans(data, _cur_choice):
-    # modified from that of CLARA
     """
-    A function to compute the configuration cost.
+    A function to compute the configuration cost. (modified from that of CLARA)
 
     :param data: The input dataframe.
     :param _cur_choice: The current set of medoid choices.
@@ -456,8 +253,8 @@ def plot_tree_clarans(data, k):
         for j in range(num_points):
             if i != j:
                 if (
-                    len(set(list(name_nodes[i])) & set(list(name_nodes[j])))
-                    == k - 1
+                        len(set(list(name_nodes[i])) & set(list(name_nodes[j])))
+                        == k - 1
                 ):
                     dot.edge(str(name_nodes[i]), str(name_nodes[j]))
 
