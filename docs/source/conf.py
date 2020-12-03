b4f0dd444f4717fbcceeb331ec6406ddb395dfb5
guglielmosanchini <guglielmosanchini@gmail.com>
2020-12-03 11:55:33 +0100

    added viewcode extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,9 @@ release = '0.0.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_autodoc_typehints']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx_autodoc_typehints',
+              'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
