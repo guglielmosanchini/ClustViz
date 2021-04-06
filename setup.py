f4b0dcf6b1aadab26304471efc430b381e6d5369
guglielmosanchini <guglielmosanchini@gmail.com>
2021-04-06 17:59:34 +0200

    bump version to 0.0.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="clustviz",
-    version="0.0.6b",
+    version="0.0.7",
     author="Guglielmo Sanchini",
     author_email="guglielmosanchini@gmail.com",
     description="A 2D clustering algorithms visualization package",
